75b776b06718d1846f3ce9ac91dc0402a40f22d6
HungryGrizzzly <mvincha404@gmail.com>
2019-09-06 20:03:41 +0300

    Revert "added base url in gradle dependent on env"

--- app/app/src/main/java/com/develop/grizzzly/pediatry/network/WebAccess.kt
+++ app/app/src/main/java/com/develop/grizzzly/pediatry/network/WebAccess.kt
@@ -1,6 +1,5 @@
 package com.develop.grizzzly.pediatry.network
 
-import com.develop.grizzzly.pediatry.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import okhttp3.OkHttpClient
@@ -9,11 +8,10 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 import java.util.*
 
-    const val DEV_URL = "https://dev.edu-pediatrics.com/api/v1/"
-const val PROD_URL = "https://edu-pediatrics.com/api/v1/"
 
 object WebAccess {
 
+    const val url = "https://edu-pediatrics.com/api/v1/"
     var token : String = ""
     var id : Long = 0
 
@@ -29,12 +27,10 @@ object WebAccess {
         val moshi = Moshi.Builder()
             .add(Date::class.java ,Rfc3339DateJsonAdapter())
             .build()
-        /**
-         * @param BuildConfig.BASE_URL прописан в файле build.gradle для каждой переменной
-         */
+
         val retrofit = Retrofit.Builder()
             .client(client)
-            .baseUrl(BuildConfig.BASE_URL)
+            .baseUrl(url)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
 
