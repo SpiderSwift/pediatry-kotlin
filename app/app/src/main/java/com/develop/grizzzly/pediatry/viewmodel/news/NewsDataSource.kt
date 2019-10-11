98b721a747b06b19533588b6d3641eafe77a59f1
clayly <clayly@gmail.com>
2019-10-11 10:37:09 +0300

    mixin logix

--- app/app/src/main/java/com/develop/grizzzly/pediatry/viewmodel/news/NewsDataSource.kt
+++ app/app/src/main/java/com/develop/grizzzly/pediatry/viewmodel/news/NewsDataSource.kt
@@ -1,6 +1,7 @@
 package com.develop.grizzzly.pediatry.viewmodel.news
 
 import android.util.Log
+import androidx.annotation.IntRange
 import androidx.paging.PositionalDataSource
 import com.develop.grizzzly.pediatry.db.DatabaseAccess
 import com.develop.grizzzly.pediatry.network.WebAccess
@@ -13,10 +14,29 @@ class NewsDataSource : PositionalDataSource<News>() {
     private val apiService = WebAccess.pediatryApi
     private val database = DatabaseAccess.database
 
+    private fun <T> mixin(
+        src: List<T>,
+        dst: MutableList<T>,
+        @Suppress("SameParameterValue")
+        @IntRange(from = 1)
+        step: Int
+    ): MutableList<T> {
+        for ((i) in src.withIndex()) {
+            try {
+                dst.add(step * (i + 1), src[i])
+            } catch (e: Exception) {
+                e.printStackTrace()
+                dst.add(src[i])
+                break
+            }
+        }
+        return dst
+    }
+
     suspend fun load(offset: Long, limit: Long) : MutableList<News> {
         if (WebAccess.offlineLog)
             WebAccess.tryLogin()
-        val ads = database.adDao().loadAds().map { it.toNews() }.toMutableList()
+        val ads = database.adDao().loadAds().map { it.toNews() }
         var news: MutableList<News>
         try {
             val responseNews = apiService.getNews(offset, limit)
@@ -30,21 +50,14 @@ class NewsDataSource : PositionalDataSource<News>() {
             e.printStackTrace()
             news = database.newsDao().getNews(offset, limit).toMutableList()
         }
-        var i = 0
-        ads.forEach {
-            i += 4
-            try {
-                news.add(i, it)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
+        mixin(ads, news, 4)
         news.forEach { Log.w("NEWS", it.toString()) }
         return news
     }
 
     override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<News>) {
         GlobalScope.launch {
+            // TODO: here must be logic for numbers calc?
             callback.onResult(load(0, 10), 0)
         }
     }
